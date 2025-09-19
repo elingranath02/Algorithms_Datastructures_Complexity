06721c3297fcecdbfae103ab49b7d496de5fcf91
timbg <timbg@kth.se>
2025-09-19 10:47:36 +0200

    Almost done

--- labb2/ClosestWords.java
+++ labb2/ClosestWords.java
@@ -6,24 +6,17 @@ import java.util.List;
 
 public class ClosestWords {
   LinkedList<String> closestWords = null;
-
+  int[][] M;
   int closestDistance = -1;
 
-  int partDist(String w1, String w2, int w1len, int w2len) {
+  int partDist(String w1, String w2, int w1len, int w2len, int index) {
     if (w1len == 0){
       return w2len;
     }
     if (w2len == 0){
       return w1len;
     }
-    int[][] M = new int[w1len+1][w2len+1];
-    for (int i = 0; i <= w1len; i++) {
-      M[i][0] = i;
-    }
-    for (int i = 0; i <= w2len; i++) {
-      M[0][i] = i;
-    }
-    for (int i = 1; i <= w1len; i++){
+    for (int i = index+1; i <= w1len; i++){
       for (int j = 1; j <= w2len; j++){
         int res = M[i-1][j-1] + (w1.charAt(i-1) == w2.charAt(j-1) ? 0 : 1);
         int deleteLetter = M[i-1][j] + 1;
@@ -40,16 +33,40 @@ public class ClosestWords {
     return M[w1len][w2len];
   }
 
-  int distance(String w1, String w2) {
-    return partDist(w1, w2, w1.length(), w2.length());
+  int distance(String w1, String w2, int numLettersAlike) {
+    return partDist(w1, w2, w1.length(), w2.length(), numLettersAlike);
   }
 
   public ClosestWords(String w, List<String> wordList) {
+    int longestWord = 0;
     for (String s : wordList) {
-      if(((s.length() >= closestDistance + w.length()) || (s.length() <= w.length() - closestDistance)) && closestDistance > -1){
+      if (s.length() > longestWord){
+        longestWord = s.length();
+      }
+    }
+    M = new int[w.length()+1][longestWord+1];
+    for (int i = 0; i <= w.length(); i++) {
+      M[i][0] = i;
+    }
+    for (int i = 0; i <= longestWord; i++) {
+      M[0][i] = i;
+    }
+    String previouString = "";
+    for (String s : wordList) {
+      if(((s.length() > closestDistance + w.length()) || (s.length() < w.length() - closestDistance)) && closestDistance > -1){
         continue;
       }
-      int dist = distance(w, s);
+      int i = 0;
+      int min = previouString.length();
+      if (s.length() < min){
+        min = s.length();
+      }
+      while (i < min){
+        if (previouString.charAt(i) != s.charAt(i)){
+          break;
+        }
+      }
+      int dist = distance(w, s, i);
       // System.out.println("d(" + w + "," + s + ")=" + dist);
       if (dist < closestDistance || closestDistance == -1) {
         closestDistance = dist;
